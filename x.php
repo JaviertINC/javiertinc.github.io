973566c66b984d19bb81bc6629daa7ad757cc3ce
@JaviertINC <luis.cortes.jaar@gmail.com>
2020-04-17 21:56:26 -0400

    Create x.php

---
+++ x.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Hacked & Defaced by @JaviertINC</title>
+<style>
+body {
+    background-image: url("https://4.bp.blogspot.com/-jIZLzxpTdQY/Vrgq1J9_A4I/AAAAAAAAAeA/L0Ni1Qn2a_g/s1600/space_zpsd3271529.gif");
+}
+._t0nqv{
+	color: #fff;
+    text-shadow: 1px 1px 2px black,
+    			0 0 25px #4caf50,
+                0 0 25px green;
+    line-height: 200px;
+    font-size: 55px;
+	vertical-align: center;
+}
+._ebbxw{
+	color: yellow;
+    text-shadow: 1px 1px 2px black,
+    			0 0 25px #4caf50,
+                0 0 25px green;
+    line-height: 10px;
+    font-size: 35px;
+    text-transform: lowercase;
+	position: relative;
+	top: -40px;
+}
+._tatht{
+	width: 650px;
+	height: 50px;
+}
+._aa7vm{
+	border: 0;
+	width: 468px;
+	height: 60px;
+}
+</style>
+</head>
+<body oncontextmenu="return false;" onkeydown="return false;" onmousedown="return false;" ondragstart="return false" onselectstart="return false" style="-moz-user-select: none; cursor: default;">
+<center>
+<div class="_t0nqv">Hacked & Defaced by @JaviertINC</div>
+<div class="_ebbxw">Your website has been hacked & Defaced</div>
+<br/>
+<br/>
+<br/>
+<img class="_tatht" src="https://2.bp.blogspot.com/-_8TZd-vMgmc/Vret4RmtCAI/AAAAAAAAAdk/EBhHeBzUuFA/s1600/hacked.gif" alt="Hacked & Defaced by @JaviertINC">
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<img class="_aa7vm" src="http://www.wieistmeineip.de/ip-address/?size=468x60">
+<br />
+<font color="green" size="5">
+	<p>Team<br>
+</font>
+<font color="green" size="5">
+	<marquee>@JaviertINC | @JhoplaINC | @YarleyINC | @Dahzek</marquee>
+</font>
+</center>
+
+<script>
+document.oncontextmenu = function(){return false}
+</script>
+</body>
+</html>
